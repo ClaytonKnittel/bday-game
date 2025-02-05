1b10cf4dcb4719efe040bfdfcc7ac8c61aecc858
Clayton Knittel <cknit1999@gmail.com>
2025-02-05 01:05:49 -0800

    Read dictionary and print highest frequency words

--- xword_gen/src/main.rs
+++ xword_gen/src/main.rs
@@ -1,12 +1,55 @@
 #![deny(clippy::unwrap_used, clippy::expect_used, clippy::panic)]
 
+use std::{
+  collections::{hash_map::Entry, HashMap},
+  fs::File,
+  io::{BufRead, BufReader},
+};
+
+use itertools::Itertools;
 use util::error::TermgameResult;
 use xword::XWord;
 
 mod dlx;
 mod xword;
 
+fn read_dict(path: &str) -> TermgameResult<HashMap<String, u32>> {
+  let mut result = HashMap::new();
+  let reader = BufReader::new(File::open(path)?);
+  for line in reader.lines() {
+    let line = line?;
+    let items: Vec<_> = line.split('\t').collect();
+    if items.len() != 4 {
+      continue;
+    }
+
+    let answer = items[2];
+    // let clue = items[3];
+
+    if answer.is_empty()
+      || !answer.chars().all(|c| c.is_alphabetic())
+      || answer.chars().all(|c| c.to_ascii_lowercase() == 'x')
+    {
+      continue;
+    }
+    match result.entry(answer.to_ascii_lowercase()) {
+      Entry::Occupied(mut entry) => *entry.get_mut() += 1,
+      Entry::Vacant(entry) => {
+        entry.insert(1);
+      }
+    }
+  }
+
+  Ok(result)
+}
+
 fn main() -> TermgameResult {
+  let dict = read_dict("clues.txt")?;
+
+  for (word, freq) in dict.iter().sorted_by_key(|(_, &freq)| !freq).take(20) {
+    println!("{word} occurs {freq} times");
+  }
+
   let xword = XWord::from_layout(
     "X___X
      _____
