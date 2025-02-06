bc02f29594e8bb8a44d423eb17bd6fc48efbc3bc
Clayton Knittel <cknit1999@gmail.com>
2025-02-05 16:25:24 -0800

    Draw crossword programmatically

--- src/crossword.rs
+++ src/crossword.rs
@@ -1,43 +1,96 @@
+use std::iter;
+
 use termgame::{color, draw::Draw, entity::Entity};
-use util::pos::Pos;
+use util::{
+  grid::{Grid, Gridlike},
+  pos::{Diff, Pos},
+};
 
 const Z_IDX: i32 = 5;
 
 pub struct Crossword {
-  w: u32,
-  h: u32,
+  grid: Grid<Option<char>>,
 }
 
 impl Crossword {
   pub fn new(w: u32, h: u32) -> Self {
-    Self { w, h }
+    Self {
+      grid: Grid::new(w, h),
+    }
+  }
+
+  pub fn width(&self) -> u32 {
+    self.grid.width()
+  }
+
+  pub fn height(&self) -> u32 {
+    self.grid.height()
   }
 }
 
 impl Entity for Crossword {
   fn iterate_tiles(&self) -> Box<dyn Iterator<Item = (Draw, Pos)> + '_> {
-    Box::new((0..self.h).flat_map(|y| {
-      (0..self.w).map(move |x| {
-        let tile = if x % 4 == 0 && y % 2 == 0 {
-          '+'
-        } else if x % 4 == 0 {
-          '|'
-        } else if y % 2 == 0 {
-          '-'
-        } else {
-          ' '
-        };
-        let col = color::AnsiValue::grayscale(20);
+    const XSCALE: i32 = 4;
+    const YSCALE: i32 = 2;
+    let col = color::AnsiValue::grayscale(20);
 
-        (
-          Draw::new(tile).with_fg(col).with_z(Z_IDX),
+    Box::new(
+      (0..self.height() as i32)
+        .flat_map(move |y| {
+          (0..self.width() as i32)
+            .flat_map(move |x| {
+              let pos = Pos {
+                x: x * XSCALE,
+                y: y * YSCALE,
+              };
+
+              (0..YSCALE).flat_map(move |dy| {
+                (0..XSCALE).map(move |dx| {
+                  let pos = pos + Diff { x: dx, y: dy };
+                  let tile = if dx == 0 && dy == 0 {
+                    '+'
+                  } else if dx == 0 {
+                    '|'
+                  } else if dy == 0 {
+                    '-'
+                  } else {
+                    ' '
+                  };
+                  (Draw::new(tile).with_fg(col).with_z(Z_IDX), pos)
+                })
+              })
+            })
+            .chain((0..YSCALE).map(move |dy| {
+              let tile = if dy == 0 { '+' } else { '|' };
+              (
+                Draw::new(tile).with_fg(col).with_z(Z_IDX),
+                Pos {
+                  x: self.width() as i32 * XSCALE,
+                  y: y * YSCALE + dy,
+                },
+              )
+            }))
+        })
+        .chain((0..self.width() as i32).flat_map(move |x| {
+          (0..XSCALE).map(move |dx| {
+            let tile = if dx == 0 { '+' } else { '-' };
+            (
+              Draw::new(tile).with_fg(col).with_z(Z_IDX),
+              Pos {
+                x: x * XSCALE + dx,
+                y: self.height() as i32 * YSCALE,
+              },
+            )
+          })
+        }))
+        .chain(iter::once((
+          Draw::new('+').with_fg(col).with_z(Z_IDX),
           Pos {
-            x: x as i32,
-            y: y as i32,
+            x: self.width() as i32 * XSCALE,
+            y: self.height() as i32 * YSCALE,
           },
-        )
-      })
-    }))
+        ))),
+    )
   }
 
   fn as_any(&self) -> &dyn std::any::Any {
